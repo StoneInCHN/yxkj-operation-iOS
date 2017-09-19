9451448cf148ea0cd1dbcf16591764e23a00ece0
lieon <lieoncx@gmail.com>
2017-09-19 14:59:46 +0800

    add project

---
+++ ContainerButler/ContainerButler/AppDelegate.swift
@@ -0,0 +1,62 @@
+//
+//  AppDelegate.swift
+//  ContainerButler
+//
+//  Created by lieon on 2017/9/11.
+//  Copyright © 2017年 QuanChengShouWei. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import IQKeyboardManagerSwift
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        setupUI()
+        chooseRootVC()
+        startLocate()
+        return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        CoreDataManager.sharedInstance.saveContext()
+    }
+}
+
+extension AppDelegate {
+    fileprivate func setupUI() {
+        UINavigationBar.appearance().barStyle = .black
+        UINavigationBar.appearance().barTintColor = UIColor.black
+        UINavigationBar.appearance().tintColor = UIColor.white
+        UINavigationBar.appearance().isTranslucent = false
+        UITabBar.appearance().barTintColor = UIColor(hex: CustomKey.Color.tabBackgroundColor)
+        UITabBar.appearance().isTranslucent = false
+        UITabBar.appearance().tintColor = UIColor(hex: CustomKey.Color.mainColor)
+        UITabBarItem.appearance().titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -3)
+        if let font = UIFont(name: "PingFangSC-Medium", size: 18) {
+            UINavigationBar.appearance().titleTextAttributes =
+                [NSForegroundColorAttributeName: UIColor.white,
+                 NSFontAttributeName: font]
+        }
+        let keyboardManager = IQKeyboardManager.sharedManager()
+        keyboardManager.enableAutoToolbar = false
+        keyboardManager.enable = true
+        keyboardManager.shouldResignOnTouchOutside = true
+        keyboardManager.shouldShowTextFieldPlaceholder = false
+    }
+    
+    fileprivate func chooseRootVC() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.white
+        window?.rootViewController = NavigationController(rootViewController: LoginViewController())
+        window?.makeKeyAndVisible()
+    }
+    
+    fileprivate func startLocate() {
+        Location.share.startLocate()
+    }
+}
