042e7f8b36d97ce1cf9a426a2224a6e65a340839
lieon <lieoncx@gmail.com>
2017-10-13 17:06:44 +0800

    修改弹窗样式

--- ContainerButler/ContainerButler/View/Common/HUD.swift
+++ ContainerButler/ContainerButler/View/Common/HUD.swift
@@ -5,6 +5,7 @@
 //  Created by lieon on 2017/9/12.
 //  Copyright © 2017年 QuanChengShouWei. All rights reserved.
 // swiftlint:disable function_parameter_count
+// swiftlint:disable legacy_constructor
 
 import UIKit
 import SVProgressHUD
@@ -18,14 +19,28 @@ class HUD {
                               enterAction: (() -> Void)?,
                               cancleAction: (() -> Void)?) {
         let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertVC.addAction(UIAlertAction(title: cancleTitle, style: .default, handler: { (_) in
-            cancleAction?()
-        }))
-        if let enter = enterTitle, !enter.isEmpty {
-            alertVC.addAction(UIAlertAction(title: enter, style: .default, handler: { (_) in
-                enterAction?()
-            }))
+        if let title = title {
+            let titleText = NSMutableAttributedString(string: title )
+            titleText.addAttributes([NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 15)], range: NSMakeRange(0, title.characters.count))
+            alertVC.setValue(titleText, forKey: "attributedTitle")
         }
+        if let message = message {
+            let messageText = NSMutableAttributedString(string: "\n" + message )
+            messageText.addAttributes([NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 13), NSAttributedStringKey.foregroundColor: UIColor(hex: 0x333333)], range: NSMakeRange(0, message.characters.count + 1))
+            alertVC.setValue(messageText, forKey: "attributedMessage")
+        }
+        let cancleAction = UIAlertAction(title: cancleTitle, style: .default, handler: { (_) in
+            cancleAction?()
+        })
+       cancleAction.setValue(UIColor(hex: 0x333333), forKey: "titleTextColor")
+        if let enter = enterTitle, !enter.isEmpty {
+            let enterAction = UIAlertAction(title: enter, style: .default, handler: { (_) in
+                enterAction?()
+            })
+            alertVC.addAction(enterAction)
+             enterAction.setValue(UIColor(hex: 0x333333), forKey: "titleTextColor")
+        }
+         alertVC.addAction(cancleAction)
         currentVC.present(alertVC, animated: true, completion: nil)
     }
     
